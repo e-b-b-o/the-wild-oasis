fb330f3a70dd95182c3bf1dff290304d4d55362a
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-04-13 22:24:17 +0300

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 40px;
+  font-weight: 600;
+`;
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The wild oasis</H1>
+      </div>
+    </>
+  );
 }
 
 export default App;
