55a7c4c8b3ce269af3fb724c8757595c8e6899fc
Ebisa Getachew <ebisagetachew1243@gmail.com>
2026-04-16 21:42:16 +0300

    added ssimple styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 40px;
   font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4r;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
+  background-color: yellow;
 `;
 
 const StyledApp = styled.main`
